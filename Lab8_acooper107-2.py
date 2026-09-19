6f365afb39a9aeb6334890a460f81e2f93533c61
Ann Cooper <wellerat@yahoo.com>
2026-09-19 10:53:25 -0400

    Initial commit

---
+++ Lab8_acooper107-2.py
@@ -0,0 +1,37 @@
+"""Geometry Calculator"""
+"""Ann Cooper"""
+"""Calulates the area and perimeter/circumference of circles and rectangles"""
+"""No started code"""
+"""Sept. 17, 2026"""
+
+
+                    
+run_program = True
+
+while run_program:
+    print("\nGeometry Calculator")
+    print("-------------------")
+    print("1. Calculate Circle Area")
+    print("2. Calculate Circle Circumfrance")
+    print("3. Calculate Rectangle Area")
+    print("4. Calculate Rectangle Perimeter")
+    print("5. Exit")
+ 
+    choice = input("\nEnter your choice (1-5)  ")
+
+    if choice == '1':
+    elif choice == '2':
+        
+    elif choice == '3':
+        
+    elif choice == '4':
+        
+    elif choice == '5':
+        print("\nGoodbye!")
+        run_program=False 
+
+    else:
+        print("\nInvalid entry, please try again....")     
+
+
+
