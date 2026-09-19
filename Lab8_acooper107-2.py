38be90b51bca6670215343a4ee7ca80f6edf3539
Ann Cooper <wellerat@yahoo.com>
2026-09-19 17:27:57 -0400

    Tool runs as expected but has not validation check on inputs for radius, height, or width

--- Lab8_acooper107-2.py
+++ Lab8_acooper107-2.py
@@ -10,6 +10,9 @@ import rectangle as r
 run_program = True
 
 while run_program:
+
+    result_message=""
+
     print("\nGeometry Calculator")
     print("-------------------")
     print("1. Calculate Circle Area")
@@ -23,27 +26,37 @@ while run_program:
     if choice == '1':
         radius = (input("\nEnter radius of circle:  "))
         area = c.calc_area(float(radius))
-        print(f"\nThe area of the circle is {area}")
+        result_message=f"\nThe area of the circle is {area}"
+
     elif choice == '2':
         radius = (input("\nEnter radius of the circle:  "))
         circumference = c.calc_circumference(float(radius))
-        print(f"\nThe circumference of the circle is {circumference}")
+        result_message=f"\nThe circumference of the circle is {circumference}"
+
     elif choice == '3':
         width = (input("\nEnter width of rectangle:  "))
         height = (input("\nEnter height of rectangle: "))
         area = r.calc_area(float(width),float(height))
-        print(f"\nThe area of the rectangle is {area}")
+        result_message=f"\nThe area of the rectangle is {area}"
+
     elif choice == '4':
         width = (input("\nEnter width of rectangle:  "))
         height = (input("\nEnter height of rectangle: "))
         perimeter = r.calc_perimeter(float(width),float(height))
-        print(f"\nThe perimeter of the rectangle is {perimeter}")
+        result_message=f"\nThe perimeter of the rectangle is {perimeter}"
+
     elif choice == '5':
         print("\nGoodbye!")
-        run_program=False 
-
+        run_program=False
+    
     else:
-        print("\nInvalid entry, please try again....")     
+        print("\nInvalid entry, please try again....") 
+
+    if result_message:
+        print(result_message)
+        input(f"\nPrint Enter to continue...")    
+
+    
 
 
 
